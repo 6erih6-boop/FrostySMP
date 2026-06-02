33e2075967afbf71461eddca761165f905c0e687
6erih6-boop <6erih6@gmail.com>
2026-06-01 23:38:54 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "FrostySMP",
         // Activity type number (0 = Playing).
         type: 0, 
       },
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "#4FC3F7", 
+      secondary: "##0F172A", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
